aab39c8d7fb71bb6a84ad6ca0e582c30ac388b56
jwcorle <JCorley111@gmail.com>
2017-06-10 13:36:20 -0400

    Added a new template

--- get_item/views.py
+++ get_item/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("<h1> Item could not be found! </h1>")
 
 def get_item_details(request, item_name):
-    return HttpResponse("Page for the item: " + item_name)
+    context = { "item_name": item_name }
+    return render(request, 'get_item/item.html', context)
