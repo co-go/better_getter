d38c8f9d3699e4abe04846e2060e9f01397c7b1f
jwcorle <JCorley111@gmail.com>
2017-06-18 12:25:29 -0400

    Adjusted css to accomodate a new navbar

--- better_getter/settings.py
+++ better_getter/settings.py
@@ -3,7 +3,7 @@ import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ 'better-getter.herokuapp.com', '127.0.0.1', 'localhost' ]
 
