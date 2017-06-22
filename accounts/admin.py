f27c3d7fa10f87a414638837bbd97c45b94116d3
jwcorle <JCorley111@gmail.com>
2017-06-22 16:10:10 -0400

    Added new admin settings

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,37 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+from .forms import CreateUserForm, ChangeUserForm
+
+
+class UserAdmin(BaseUserAdmin):
+
+    form = UserChangeForm
+    add_form = CreateUserForm
+    # don't need to override change_password_form
+
+    list_display = ('username', 'is_superuser')
+    list_filter = ()
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('username',)}),
+        ('Permissions', {'fields': ('is_superuser')}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': ('username', 'password1', 'password2')
+            }
+        ),
+    )
+    search_fields = ('username',)
+    ordering = ('username',)
+    filter_horizontal = ()
+
+admin.site.register(User, UserAdmin)
